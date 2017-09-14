76ac6ffd33222fa74045db053f93be7226e05855
Alexey Seleznev <selesnow@mail.ru>
2017-09-14 15:10:04 +0300

    Update vkAuth.R

--- R/vkAuth.R
+++ R/vkAuth.R
@@ -3,7 +3,7 @@ vkAuth <- function(app_id = NULL, client_secret = NULL, apiVersion = "5.68"){
   browseURL(paste0("https://oauth.vk.com/authorize?client_id=",app_id,"&redirect_uri=https://oauth.vk.com/blank.html&display=popup&scope=offline,groups,ads,stats&response_type=code&v=",apiVersion))
   code <- readline(prompt = "Enter code from URL: ")
   #Ïîëó÷àåì òîêåí
-  token <- GET(paste0("https://oauth.vk.com/access_token?client_id=",client_id,"&client_secret=",client_secret,"&redirect_uri=https://oauth.vk.com/blank.html&code=",code))
+  token <- GET(paste0("https://oauth.vk.com/access_token?client_id=",app_id,"&client_secret=",client_secret,"&redirect_uri=https://oauth.vk.com/blank.html&code=",code))
   stop_for_status(token)
   accessCredentilas <- content(token, "parsed", "application/json")
   save(accessCredentilas, file = "vkauth.RData")
