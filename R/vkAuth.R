f6a11a9c0af069b3ae67ed16a138356fbd66baab
Alexey Seleznev <selesnow@mail.ru>
2017-09-14 14:09:26 +0300

    Update vkAuth.R

--- R/vkAuth.R
+++ R/vkAuth.R
@@ -1,8 +1,8 @@
-vkAuth <- function(client_id = NULL, client_secret = NULL, apiVersion = "5.68"){
-  #Ïîëó÷àåì code
-  browseURL(paste0("https://oauth.vk.com/authorize?client_id=",client_id,"&redirect_uri=https://oauth.vk.com/blank.html&display=popup&scope=offline,groups,ads,stats&response_type=code&v=",apiVersion))
+vkAuth <- function(app_id = NULL, client_secret = NULL, apiVersion = "5.68"){
+  #ÃÃ®Ã«Ã³Ã·Ã Ã¥Ã¬ code
+  browseURL(paste0("https://oauth.vk.com/authorize?client_id=",app_id,"&redirect_uri=https://oauth.vk.com/blank.html&display=popup&scope=offline,groups,ads,stats&response_type=code&v=",apiVersion))
   code <- readline(prompt = "Enter code from URL: ")
-  #Ïîëó÷àåì òîêåí
+  #ÃÃ®Ã«Ã³Ã·Ã Ã¥Ã¬ Ã²Ã®ÃªÃ¥Ã­
   token <- GET(paste0("https://oauth.vk.com/access_token?client_id=",client_id,"&client_secret=",client_secret,"&redirect_uri=https://oauth.vk.com/blank.html&code=",code))
   stop_for_status(token)
   accessCredentilas <- content(token, "parsed", "application/json")
