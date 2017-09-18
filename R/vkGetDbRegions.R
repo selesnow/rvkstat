2f061fa7505a476851b8af5a76842710c9a71ab3
Alexey Seleznev <selesnow@mail.ru>
2017-09-18 15:40:59 +0300

    Add files via upload

--- R/vkGetDbRegions.R
+++ R/vkGetDbRegions.R
@@ -1,23 +1,16 @@
-vkGetDbCities <- function(country_id = TRUE,
-                          region_id = NULL,
-                          q = NULL,
-                          need_all = TRUE,
-                          access_token = NULL){
+vkGetDbRegions <- function(country_id = NULL,
+                           q = NULL,
+                           access_token = NULL){
 
   if(is.null(access_token)){
     stop("Не заполнен access_token, этот аргумент является обязательным.")
   }
   
-  #Фильтр по статусу объявления
-  if(is.null(country_id)){
-  stop("Не заполнен country_id, этот аргумент является обязательным.")
-  }
   
-    #Фильтр по статусу объявления
+  #Фильтр по статусу объявления
   if(nchar(q) > 15 && !(is.null(q))){
   stop(paste0("В аргументе q максимальная длина строки — 15 символов. Вы ввели щапрос состоящий из ", nchar(q)," символов!"))
   }
-  
   
   if(need_all == TRUE){
     need_all <- 1
@@ -37,7 +30,7 @@ vkGetDbCities <- function(country_id = TRUE,
   while(last_iteration == FALSE){
 
   #Формируем запрос
-  query <- paste0("https://api.vk.com/method/database.getCities?need_all=",need_all,"&country_id=",country_id,ifelse(!(is.null(region_id)),paste0("&region_id=",region_id),""),ifelse(!(is.null(q)),paste0("&q=",q),""),"&offset=",offset,"&count=",count,"&access_token=",access_token)
+  query <- paste0("https://api.vk.com/method/database.getRegions?need_all=",need_all,"&country_id=",country_id,ifelse(!(is.null(region_id)),paste0("&region_id=",region_id),""),ifelse(!(is.null(q)),paste0("&q=",q),""),"&offset=",offset,"&count=",count,"&access_token=",access_token)
   answer <- GET(query)
   stop_for_status(answer)
   dataRaw <- content(answer, "parsed", "application/json")
@@ -50,11 +43,8 @@ vkGetDbCities <- function(country_id = TRUE,
   #Парсинг результата
   for(i in 1:length(dataRaw$response)){
     result  <- rbind(result,
-                     data.frame(cid                  = ifelse(is.null(dataRaw$response[[i]]$cid), NA,dataRaw$response[[i]]$cid),
+                     data.frame(region_id            = ifelse(is.null(dataRaw$response[[i]]$region_id), NA,dataRaw$response[[i]]$region_id),
                                 title                = ifelse(is.null(dataRaw$response[[i]]$title), NA,dataRaw$response[[i]]$title),
-                                important            = ifelse(is.null(dataRaw$response[[i]]$important), NA,dataRaw$response[[i]]$important),
-                                area                 = ifelse(is.null(dataRaw$response[[i]]$area), NA,dataRaw$response[[i]]$area),
-                                region               = ifelse(is.null(dataRaw$response[[i]]$region), NA,dataRaw$response[[i]]$region),
                                 stringsAsFactors = F))}
   
   if(length(dataRaw$response) < 1000){
@@ -66,3 +56,4 @@ vkGetDbCities <- function(country_id = TRUE,
   
   return(result)
 }
+  
