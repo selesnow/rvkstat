cba7bc112689fa9923cd82182cd8ab9b889fa74b
Alexey Seleznev <selesnow@mail.ru>
2017-09-18 11:01:14 +0300

    Add files via upload

---
+++ R/vkGetAdGenderStats.R
@@ -0,0 +1,105 @@
+vkGetAdGenderStats <- function(account_id = NULL,
+                               ids_type = "campaign",
+                               ids = NULL,
+                               period = "day",
+                               date_from = Sys.Date() - 30,
+                               date_to = Sys.Date(),
+                               access_token = NULL){
+  if(is.null(access_token)){
+    stop("Не заполнен access_token, этот аргумент является обязательным.")
+  }
+  
+  if(!(period %in% c("day","month","overall"))){
+    stop("Неверное значение в аргументе period, доступные значения day, month и overall")
+  }
+  
+  #Переводим дату в месяц
+  if(period == "month"){
+    date_from <- format(as.Date(as.character(date_from)), "%Y-%m")
+    date_to   <- format(as.Date(as.character(date_to)), "%Y-%m")
+  }
+  
+  if(period == "overall"){
+    date_from <- 0
+    date_to   <- 0
+  }
+  
+  #Фильтр по статусу объявления
+  ids <- paste0(ids, collapse = ",")
+  
+  #Рещультирующая таблица
+  result <- data.frame()  
+  
+  #Формируем запрос
+  query <- paste0("https://api.vk.com/method/ads.getDemographics?account_id=",account_id,"&ids_type=",ids_type,"&ids=",ids,"&period=",period,"&date_from=",date_from,"&date_to=",date_to,"&access_token=",access_token)
+  answer <- GET(query)
+  stop_for_status(answer)
+  dataRaw <- content(answer, "parsed", "application/json")
+  
+  #Проверка ответа на ошибки
+  if(!is.null(dataRaw$error)){
+    stop(paste0("Error ", dataRaw$error$error_code," - ", dataRaw$error$error_msg))
+  }
+  
+  for(i in 1:length(dataRaw$response)){
+    
+    #Парсинг результата
+    if(period == "day"){
+      for(dt in 1:length(dataRaw$response[[i]]$stats)){
+        
+        for(city_i in 1:length(dataRaw$response[[i]]$stats[[dt]]$sex)){
+          
+          if(length(dataRaw$response[[i]]$stats[[dt]]$sex)==0) next
+          
+        result  <- rbind(result,
+                         data.frame(id                  = ifelse(is.null(dataRaw$response[[i]]$id), NA,dataRaw$response[[i]]$id),
+                                    type                = ifelse(is.null(dataRaw$response[[i]]$type), NA,dataRaw$response[[i]]$type),
+                                    day                 = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$day), NA,dataRaw$response[[i]]$stats[[dt]]$day),
+                                    gender              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$value), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$value),
+                                    impressions_rate    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$impressions_rate), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$impressions_rate),
+                                    clicks_rate         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$clicks_rate), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$clicks_rate),
+                                    stringsAsFactors = F))}
+      }
+    }
+
+    if(period == "month"){
+      for(dt in 1:length(dataRaw$response[[i]]$stats)){
+        
+        for(city_i in 1:length(dataRaw$response[[i]]$stats[[dt]]$sex)){
+          
+          if(length(dataRaw$response[[i]]$stats[[dt]]$sex)==0) next
+
+        result  <- rbind(result,
+                         data.frame(id                  = ifelse(is.null(dataRaw$response[[i]]$id), NA,dataRaw$response[[i]]$id),
+                                    type                = ifelse(is.null(dataRaw$response[[i]]$type), NA,dataRaw$response[[i]]$type),
+                                    month               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$month), NA,dataRaw$response[[i]]$stats[[dt]]$month),
+                                    gender              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$value), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$value),
+                                    impressions_rate    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$impressions_rate), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$impressions_rate),
+                                    clicks_rate         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$clicks_rate), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$clicks_rate),
+                                    stringsAsFactors = F))}
+    }
+  }
+    if(period == "overall"){
+      for(dt in 1:length(dataRaw$response[[i]]$stats)){
+        
+        for(city_i in 1:length(dataRaw$response[[i]]$stats[[dt]]$sex)){
+          
+          if(length(dataRaw$response[[i]]$stats[[dt]]$sex)==0) next
+          
+          result  <- rbind(result,
+                           data.frame(id                  = ifelse(is.null(dataRaw$response[[i]]$id), NA,dataRaw$response[[i]]$id),
+                                      type                = ifelse(is.null(dataRaw$response[[i]]$type), NA,dataRaw$response[[i]]$type),
+                                      day_from            = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$day_from), NA,dataRaw$response[[i]]$stats[[dt]]$day_from),
+                                      day_to              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$day_to), NA,dataRaw$response[[i]]$stats[[dt]]$day_to),
+                                      gender              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$value), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$value),
+                                      impressions_rate    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$impressions_rate), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$impressions_rate),
+                                      clicks_rate         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$clicks_rate), NA,dataRaw$response[[i]]$stats[[dt]]$sex[[city_i]]$clicks_rate),
+                                      stringsAsFactors = F))}
+      }
+    }
+    
+  }
+  
+  
+  return(result)
+}
