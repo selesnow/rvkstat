709cd738b63a63c0f170b9f3dab72dd4fd27b6d4
Alexey Seleznev <selesnow@mail.ru>
2017-09-22 13:02:51 +0300

    Update vkGetToken.R

--- R/vkGetToken.R
+++ R/vkGetToken.R
@@ -1,6 +1,6 @@
 vkGetToken <-
 function(app_id = NULL) {
-tokenURL <- paste0("https://oauth.vk.com/authorize?client_id=",app_id,"&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=offline,groups,ads,stats&response_type=token&v=5.68")
+tokenURL <- paste0("https://oauth.vk.com/authorize?client_id=",app_id,"&display=page&redirect_uri=https://selesnow.github.io/rvkstat/getCode/get_token.html&scope=offline,groups,ads,stats&response_type=token&v=5.68")
 browseURL(tokenURL)
 vkToken <- readline("Your vk token from URL parameter access_token: ")
 save(vkToken, file = "vkauth")
