957c73345edb2603bc664b422dd664a55c862c4b
Alexey Seleznev <selesnow@mail.ru>
2019-01-17 15:10:29 +0200

    Add files via upload

--- R/vkGetAdStatistics.R
+++ R/vkGetAdStatistics.R
@@ -85,15 +85,15 @@ vkGetAdStatistics <- function(account_id = NULL,
                      data.frame(id                  = ifelse(is.null(dataRaw$response[[i]]$id), NA,dataRaw$response[[i]]$id),
                                 type                = ifelse(is.null(dataRaw$response[[i]]$type), NA,dataRaw$response[[i]]$type),
                                 day                 = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$day), NA,dataRaw$response[[i]]$stats[[dt]]$day),
-                                spent               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$spent), 0,dataRaw$response[[i]]$stats[[dt]]$spent),
-                                impressions         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$impressions), 0,dataRaw$response[[i]]$stats[[dt]]$impressions),
-                                clicks              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$clicks), 0,dataRaw$response[[i]]$stats[[dt]]$clicks),
-                                reach               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$reach), 0,dataRaw$response[[i]]$stats[[dt]]$reach),
+                                spent               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$spent), NA,dataRaw$response[[i]]$stats[[dt]]$spent),
+                                impressions         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$impressions), NA,dataRaw$response[[i]]$stats[[dt]]$impressions),
+                                clicks              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$clicks), NA,dataRaw$response[[i]]$stats[[dt]]$clicks),
+                                reach               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$reach), NA,dataRaw$response[[i]]$stats[[dt]]$reach),
                                 video_views         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views), NA,dataRaw$response[[i]]$stats[[dt]]$video_views),
                                 video_views_half    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views_half), NA,dataRaw$response[[i]]$stats[[dt]]$video_views_half),
                                 video_views_full    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views_full), NA,dataRaw$response[[i]]$stats[[dt]]$video_views_full),
                                 video_clicks_site   = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_clicks_site), NA,dataRaw$response[[i]]$stats[[dt]]$video_clicks_site),
-                                join_rate           = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$join_rate ), 0,dataRaw$response[[i]]$stats[[dt]]$join_rate),
+                                join_rate           = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$join_rate ), NA,dataRaw$response[[i]]$stats[[dt]]$join_rate),
                                 stringsAsFactors = F))}
     }
    if(period == "month"){
@@ -103,15 +103,15 @@ vkGetAdStatistics <- function(account_id = NULL,
                       data.frame(id                  = ifelse(is.null(dataRaw$response[[i]]$id), NA,dataRaw$response[[i]]$id),
                                  type                = ifelse(is.null(dataRaw$response[[i]]$type), NA,dataRaw$response[[i]]$type),
                                  month               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$month), NA,dataRaw$response[[i]]$stats[[dt]]$month),
-                                 spent               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$spent), 0,dataRaw$response[[i]]$stats[[dt]]$spent),
-                                 impressions         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$impressions), 0,dataRaw$response[[i]]$stats[[dt]]$impressions),
-                                 clicks              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$clicks), 0,dataRaw$response[[i]]$stats[[dt]]$clicks),
-                                 reach               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$reach), 0,dataRaw$response[[i]]$stats[[dt]]$reach),
+                                 spent               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$spent), NA,dataRaw$response[[i]]$stats[[dt]]$spent),
+                                 impressions         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$impressions), NA,dataRaw$response[[i]]$stats[[dt]]$impressions),
+                                 clicks              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$clicks), NA,dataRaw$response[[i]]$stats[[dt]]$clicks),
+                                 reach               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$reach), NA,dataRaw$response[[i]]$stats[[dt]]$reach),
                                  video_views         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views), NA,dataRaw$response[[i]]$stats[[dt]]$video_views),
                                  video_views_half    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views_half), NA,dataRaw$response[[i]]$stats[[dt]]$video_views_half),
                                  video_views_full    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views_full), NA,dataRaw$response[[i]]$stats[[dt]]$video_views_full),
                                  video_clicks_site   = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_clicks_site), NA,dataRaw$response[[i]]$stats[[dt]]$video_clicks_site),
-                                 join_rate           = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$join_rate ), 0,dataRaw$response[[i]]$stats[[dt]]$join_rate),
+                                 join_rate           = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$join_rate ), NA,dataRaw$response[[i]]$stats[[dt]]$join_rate),
                                  stringsAsFactors = F))}
    }
    if(period == "overall"){
@@ -122,23 +122,27 @@ vkGetAdStatistics <- function(account_id = NULL,
                                     type                = ifelse(is.null(dataRaw$response[[i]]$type), NA,dataRaw$response[[i]]$type),
                                     day_from            = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$day_from), NA,dataRaw$response[[i]]$stats[[dt]]$day_from),
                                     day_to              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$day_to), NA,dataRaw$response[[i]]$stats[[dt]]$day_to),
-                                    spent               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$spent), 0,dataRaw$response[[i]]$stats[[dt]]$spent),
-                                    impressions         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$impressions), 0,dataRaw$response[[i]]$stats[[dt]]$impressions),
-                                    clicks              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$clicks), 0,dataRaw$response[[i]]$stats[[dt]]$clicks),
-                                    reach               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$reach), 0,dataRaw$response[[i]]$stats[[dt]]$reach),
+                                    spent               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$spent), NA,dataRaw$response[[i]]$stats[[dt]]$spent),
+                                    impressions         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$impressions), NA,dataRaw$response[[i]]$stats[[dt]]$impressions),
+                                    clicks              = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$clicks), NA,dataRaw$response[[i]]$stats[[dt]]$clicks),
+                                    reach               = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$reach), NA,dataRaw$response[[i]]$stats[[dt]]$reach),
                                     video_views         = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views), NA,dataRaw$response[[i]]$stats[[dt]]$video_views),
                                     video_views_half    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views_half), NA,dataRaw$response[[i]]$stats[[dt]]$video_views_half),
                                     video_views_full    = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_views_full), NA,dataRaw$response[[i]]$stats[[dt]]$video_views_full),
                                     video_clicks_site   = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$video_clicks_site), NA,dataRaw$response[[i]]$stats[[dt]]$video_clicks_site),
-                                    join_rate           = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$join_rate ), 0,dataRaw$response[[i]]$stats[[dt]]$join_rate),
+                                    join_rate           = ifelse(is.null(dataRaw$response[[i]]$stats[[dt]]$join_rate ), NA,dataRaw$response[[i]]$stats[[dt]]$join_rate),
                                     stringsAsFactors = F))}
      }
     }
   }
   
   # data type mismatch handler 
-  result$spent <- as.numeric(result$spent)
-  if (length(result$clicks)>0) result$clicks <- as.integer(result$clicks)
-  if (length(result$join_rate)>0) result$join_rate <- as.integer(result$join_rate)
+  if (nrow(result) > 0) {
+    result$spent <- as.numeric(result$spent)
+    result$clicks <- as.integer(result$clicks)
+    result$impressions <- as.integer(result$clicks)
+    result$reach <- as.integer(result$clicks)
+    result$join_rate <- as.integer(result$join_rate)
+  }
   return(result)
 }
